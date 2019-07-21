aded2af89789d05ae2cadca54f0a19f30d0254fd
smitakolnure <smitakolnure0508@gmail.com>
2019-07-22 01:54:15 +0530

    Massage_send through Fragment

---
+++ app/src/main/java/com/ab/fragmentexampleone/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ab.fragmentexampleone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements MessageFragment.OnMessageReadListener {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.fragment_container)!=null)
+        {
+            if(savedInstanceState!=null)
+            {
+                return;
+            }
+
+            MessageFragment messageFragment= new MessageFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,messageFragment,null);
+            fragmentTransaction.commit();
+        }
+
+    }
+
+    @Override
+    public void onread(String message) {
+
+        textView= findViewById(R.id.txt_display);
+        textView.setText(message);
+    }
+}
